62bcde08a2f94fc480e1900bd22eb0bdf7c21b06
Nitesh <imnksid@gmail.com>
2026-06-17 02:37:30 +0700

    Added necessary images

---
+++ src/assets/images/index.ts
@@ -0,0 +1,7 @@
+export const IMG_SPLASH_IMAGE = require("./splashImage.png");
+
+export const IMG_WELCOME_IMAGE = require("./welcome.png");
+
+export const IMG_CARD_IMAGE = require("./card.png");
+
+export const IMG_DEFAULT_AVATAR = require("./defaultAvatar.png");
