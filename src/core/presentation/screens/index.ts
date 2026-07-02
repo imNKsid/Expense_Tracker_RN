9a0370c5b25dcc70157a20a35399a9a9bca93feb
Nitesh <imnksid@gmail.com>
2026-07-02 14:23:34 +0700

    Created bottom tab & added  screens - Home, Statistics, Wallet & Profile

--- src/core/presentation/screens/index.ts
+++ src/core/presentation/screens/index.ts
@@ -3,4 +3,18 @@ import LoginScreen from "./login/LoginScreen";
 import RegisterScreen from "./register/RegisterScreen";
 import WelcomeScreen from "./welcome/WelcomeScreen";
 
-export { LoginScreen, RegisterScreen, WelcomeScreen };
+// Bottom Tab screens
+import HomeScreen from "./home/HomeScreen";
+import ProfileScreen from "./profile/ProfileScreen";
+import StatisticsScreen from "./statistics/StatisticsScreen";
+import WalletScreen from "./wallet/WalletScreen";
+
+export {
+  HomeScreen,
+  LoginScreen,
+  ProfileScreen,
+  RegisterScreen,
+  StatisticsScreen,
+  WalletScreen,
+  WelcomeScreen,
+};
